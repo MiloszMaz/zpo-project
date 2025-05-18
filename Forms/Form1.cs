186e9f377db5d364ab758779eb6b3b78ae67ccf6
Miłosz Mazurewicz <biuro@codefbs.pl>
2025-05-18 13:01:32 +0200

    feat<error-handling> add exception handling in data download

--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -1,4 +1,5 @@
 using projekt.Dto;
+using projekt.Exceptions;
 using projekt.Services;
 
 namespace projekt
@@ -15,14 +16,33 @@ namespace projekt
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                this.downloadData();
+            }
+            catch (FileException exception)
+            {
+                MessageBox.Show(exception.Message, "FileException");
+            }
+            catch (ApiException exception)
+            {
+                MessageBox.Show(exception.Message, "ApiException");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Exception");
+            }
+            finally
+            {
+                MessageBox.Show("Data saved.", "Success");
+            }
+        }
+
+        private void downloadData()
+        {
             List<ExchangeRate> list = _nbpService.getExchangeRates();
 
             _fileService.save(list);
-
-            //foreach (var item in list)
-            //{
-            //    MessageBox.Show(item.currency);
-            //}
         }
     }
 }
--- obj/Debug/net8.0-windows/projekt.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/projekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba8af09519c45aee01baf5ec623985636522b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed6239bef83cb4733b2551487b9fbfe06e36bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
